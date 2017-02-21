56278b26e106a2ff1a66d95ab062a6987e16aeba
kiran <kj.khaladu@gmail.com>
2017-02-21 23:04:43 +0530

    replace no n from array in place

---
+++ python/replace_n.py
@@ -0,0 +1,19 @@
+#!/bin/python3
+
+def replace(n, array):
+    end = len(array) - 1
+    i = 0
+    while True:
+        if array[i] == n:
+            if array[end] == n:
+                while array[end] == n:
+                    end -= 1
+            temp = array[end]
+            array[end] = array[i]
+            array[i] = temp
+            i += 1
+        if i == end:
+            break
+    return end
+
+print(replace(2, [4, 3, 2, 1, 2, 3, 6]))
